b2eafde8e489e940baf7debb16326786b6784b53
ricardo <ricardo.petri@unifil.br>
2025-02-25 22:08:17 -0300

    Implementação remoção lista dinâmica

--- src/ListaDinamica.java
+++ src/ListaDinamica.java
@@ -23,4 +23,50 @@ public class ListaDinamica {
             return false;
         }
     }
+
+    public void inserirValor(String conteudo) {
+        No novoNo = new No(conteudo);
+        if(estaVazio()) {
+            this.primeiro = novoNo;
+        } else {
+            No aux = this.primeiro;
+            while (aux.getProx() != null) {
+                aux = aux.getProx();
+            }
+            aux.setProx(novoNo);
+        }
+    }
+
+    public boolean verificaExistencia(String conteudo) {
+        if(estaVazio()) {
+            return false;
+        } else {
+            No aux = this.primeiro;
+            while(aux != null) {
+                if(aux.getConteudo() == conteudo) {
+                    return true;
+                }
+                aux = aux.getProx();
+            }
+            return false;
+        }
+    }
+
+    public void removerValor(String conteudo) {
+        No aux = this.primeiro;
+        if(this.primeiro.getConteudo() == conteudo) {//Removendo primeiro elemento
+            this.primeiro = this.primeiro.getProx();
+        } else {
+            if(verificaExistencia(conteudo)){
+                while (aux != null) {
+                    if(aux.getProx().getConteudo() == conteudo) {
+                        aux.setProx(aux.getProx().getProx());
+                        return;
+                    }
+                    aux = aux.getProx();
+                }
+            }
+            System.out.println("O elemento não existe.");
+        }
+    }
 }
--- src/MainListaDinamica.java
+++ src/MainListaDinamica.java
@@ -1,6 +1,11 @@
 public class MainListaDinamica {
     public static void main(String[] args) {
         ListaDinamica turma = new ListaDinamica("Ysadora");
+
+        turma.inserirValor("Isabely");
+        turma.inserirValor("Jorge");
+        turma.inserirValor("Eduardo");
+        turma.removerValor("Eduardo");
         turma.verLista();
     }
 }
